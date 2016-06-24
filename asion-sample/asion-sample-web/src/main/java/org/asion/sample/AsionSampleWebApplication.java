9aa42e9688b953ca1ceed0197fb47c9692db85e8
Asion <luxuexian99@gmail.com>
2016-06-24 17:54:21 +0800

    Sample Multipart limit.

--- asion-sample/asion-sample-web/src/main/java/org/asion/sample/AsionSampleWebApplication.java
+++ asion-sample/asion-sample-web/src/main/java/org/asion/sample/AsionSampleWebApplication.java
@@ -5,6 +5,7 @@ import org.sitemesh.config.ConfigurableSiteMeshFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.servlet.DispatcherType;
+import javax.servlet.MultipartConfigElement;
 import java.util.EnumSet;
 
 /**
@@ -54,6 +56,17 @@ public class AsionSampleWebApplication extends WebMvcConfigurerAdapter {
             registry.addResourceHandler("/webjars/**").addResourceLocations(
                     "classpath:/META-INF/resources/webjars/");
         }
+    }
+
+    /**
+     * Multipart limit
+     */
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize("10240KB");
+        factory.setMaxRequestSize("10240KB");
+        return factory.createMultipartConfig();
     }
 
 }
