5b3b702c204388257e709f79fa45ed351f23e83b
Asion <luxx@ytx.com>
2016-11-11 15:49:08 +0800

    Sample AssertJ.

---
+++ asion-sample/asion-sample-core/src/test/java/org/asion/sample/common/DateUtil.java
@@ -0,0 +1,21 @@
+package org.asion.sample.common;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+/**
+ * @author Asion.
+ * @since 16/7/2.
+ */
+public final class DateUtil {
+
+    public static Date localDateTimeToDate(LocalDateTime localDateTime) {
+        return Date.from(localDateTime.toInstant(ZoneOffset.UTC));
+    }
+
+    public static LocalDateTime dateToLocalDateTime(Date date) {
+        return LocalDateTime.ofInstant(Instant.ofEpochMilli(date.getTime()), ZoneOffset.UTC);
+    }
+}
