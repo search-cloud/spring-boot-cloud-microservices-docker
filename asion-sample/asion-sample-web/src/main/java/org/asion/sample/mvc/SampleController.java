29f05572fedfd937fe2f282a00f1477fa0c5ae09
Asion <luxuexian99@gmail.com>
2017-03-13 22:52:31 +0800

    update mapping.

--- asion-sample/asion-sample-web/src/main/java/org/asion/sample/mvc/SampleController.java
+++ asion-sample/asion-sample-web/src/main/java/org/asion/sample/mvc/SampleController.java
@@ -5,10 +5,7 @@ import org.asion.sample.SampleManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -26,24 +23,24 @@ public class SampleController {
     @Autowired
     private SampleManager sampleManager;
 
-    @RequestMapping("list")
+    @GetMapping("list")
     public ModelAndView list() {
         Iterable<Sample> samples = sampleManager.findAll();
         return new ModelAndView("sample/list", "samples", samples);
     }
 
-    @RequestMapping("{id}")
+    @GetMapping("{id}")
     public ModelAndView view(@PathVariable("id") Long id) {
         Sample sample = sampleManager.findOne(id);
         return new ModelAndView("sample/view", "sample", sample);
     }
 
-    @RequestMapping(params = "form", method = RequestMethod.GET)
+    @GetMapping(params = "form")
     public String createForm(@ModelAttribute Sample sample) {
         return "sample/form";
     }
 
-    @RequestMapping(value = "create",method = RequestMethod.POST)
+    @PostMapping(value = "create")
     public ModelAndView create(@Valid Sample sample, BindingResult result, RedirectAttributes redirect) {
         if (result.hasErrors()) {
             return new ModelAndView("sample/form", "formErrors", result.getAllErrors());
@@ -53,19 +50,19 @@ public class SampleController {
         return new ModelAndView("redirect:/sample/{sample.id}", "sample.id", sample.getId());
     }
 
-    @RequestMapping("foo")
+    @GetMapping("foo")
     public String foo() {
         throw new RuntimeException("Expected exception in controller");
     }
 
-    @RequestMapping(value = "delete/{id}")
+    @DeleteMapping(value = "delete/{id}")
     public ModelAndView delete(@PathVariable("id") Long id) {
         sampleManager.delete(id);
         Iterable<Sample> samples = sampleManager.findAll();
         return new ModelAndView("sample/list", "samples", samples);
     }
 
-    @RequestMapping(value = "modify/{id}", method = RequestMethod.GET)
+    @GetMapping(value = "modify/{id}")
     public ModelAndView modifyForm(@PathVariable("id") Long id) {
         Sample sample = sampleManager.findOne(id);
         return new ModelAndView("sample/form", "sample", sample);
